69e586f5cb2203ed926de197b2e62cd2c984e58e
Hannah Mundt <mundth19@hanover.edu>
2018-05-20 14:44:50 -0400

    finished joke

--- app/src/main/java/edu/hanover/joke/MainActivity.java
+++ app/src/main/java/edu/hanover/joke/MainActivity.java
@@ -1,13 +1,19 @@
 package edu.hanover.joke;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+import android.view.View;
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        Intent intent = new Intent(this, DelayedMessageService.class);
+        intent.putExtra(DelayedMessageService.EXTRA_MESSAGE,
+                getResources().getString(R.string.button_response));
+        startService(intent);
     }
 }
